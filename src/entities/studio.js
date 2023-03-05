fff36d588d3fb10f7650cb9bf765d410617f2a1a
vasilii <porokh.vs@gmail.com>
2023-03-05 18:40:41 +0300

    process

--- src/entities/studio.js
+++ src/entities/studio.js
@@ -7,23 +7,19 @@ export const createStudio = () => {
         canvas: document.getElementById( 'webgl-canvas' ),
         antialias: true,
     })
-    //renderer.setClearColor(0x000000)
     renderer.setClearColor(0x333333)
     renderer.setPixelRatio( window.devicePixelRatio)
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-    //renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
     const scene = new THREE.Scene()
 
-    const lightA = new THREE.AmbientLight( 0xffffff, 3)
-    lightA.position.set( 5, 5, 5 )
+    const lightA = new THREE.AmbientLight( 0xffffff, 2)
     scene.add( lightA )
 
-
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 2 )
-    dirLight.position.set( 0, 500, 200)
+    const dirLight = new THREE.DirectionalLight( 0xffffff, 5)
+    dirLight.position.set(0, 300, 200)
     dirLight.castShadow = true
     dirLight.shadow.camera.top = 500
     dirLight.shadow.camera.bottom = -500
@@ -34,9 +30,9 @@ export const createStudio = () => {
     scene.add(dirLight)
 
     const ground = new THREE.Mesh(
-        new THREE.PlaneGeometry( 1000, 1000, 1, 1 ),
-        new THREE.ShadowMaterial( { color: 0x000011, opacity: .3, side: THREE.DoubleSide } )
-    );
+        new THREE.PlaneGeometry(1000, 1000, 1, 1),
+        new THREE.ShadowMaterial( { color: 0x000011, opacity: .3, side: THREE.DoubleSide })
+    )
 
     ground.rotation.x = - Math.PI / 2
     ground.position.y = -1
@@ -48,13 +44,14 @@ export const createStudio = () => {
     //scene.add(helper)
 
 
-
-    scene.fog = new THREE.Fog(0x000000, 300, 1500)
+    scene.fog = new THREE.Fog(0x000000, 300, 3000)
 
     const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 10000)
-    camera.position.set(0, 200, 200)
+    camera.position.set(100, 200, 200)
+    camera.lookAt(50, 20, 0)
 
     const controls = new OrbitControls(camera, renderer.domElement)
+    controls.target.set(70, 50, 0)
     controls.update();
 
 
@@ -76,15 +73,12 @@ export const createStudio = () => {
                 return;
             }
             count += 0.001
-            dirLight.position.x = Math.sin(count) * 300
-            //dirLight.position.z = Math.cos(count) * 300
+            dirLight.position.x = Math.sin(count) * 150 - 300
+            dirLight.position.z = Math.cos(count) * 150 + 200
             renderer.render(scene, camera)
         },
         addToScene: mesh => {
             scene.add(mesh)
         },
-        setBack: back => {
-            //scene.background = back
-        }
     }
 }
